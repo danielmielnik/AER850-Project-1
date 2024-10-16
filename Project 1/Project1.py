04d1fa0bb29a354d5e33543071b393df68004163
danielmielnik <127531735+danielmielnik@users.noreply.github.com>
2024-10-16 16:33:40 -0400

    Update Project1.py

--- Project 1/Project1.py
+++ Project 1/Project1.py
@@ -1,27 +1,31 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sb
 
 # Step 1: Data Processing
 df = pd.read_csv('Project_1_Data.csv')
 print(df)
 
 # Step 2: Data Visualization
+# Individual Steps
 for i in range(1, 14):
     step = df.loc[df['Step'] == i]
     print(step)
     
     X = step.get("X")
     Y = step.get("Y")
-    Z = step['Z'].values.reshape(-1, 1) 
+    Z = step.get("Z")
     
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     
-    ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
+    ax.scatter(X, Y, Z)
+    # Add same axis for each
     
     plt.show()
 
+# Combined Steps
 X = df['X']
 Y = df['Y']
 Z = df['Z'] 
@@ -31,3 +35,14 @@ ax = fig.add_subplot(projection='3d')
 
 ax.scatter(X, Y, Z)
 
+# Step 3: Correlation Analysis
+correlation = df.corr()
+print(correlation)
+
+plt.figure()
+sb.heatmap(correlation)
+
+# Step 4: Classification Model Development/Engineering
+coordinates = df[['X', 'Y', 'Z']]
+step = df['Step']
+print(coordinates, step)
