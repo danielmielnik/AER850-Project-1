8584788a934b1f51a38713fb36114ae9a8cb2b3a
danielmielnik <127531735+danielmielnik@users.noreply.github.com>
2024-10-19 11:29:01 -0400

    Version X

--- Project 1/Project1.py
+++ Project 1/Project1.py
@@ -7,8 +7,8 @@ from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_absolute_error #remove
+from sklearn.model_selection import GridSearchCV 
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 import joblib
 
@@ -60,31 +60,32 @@ coord_train, coord_test, step_train, step_test = train_test_split(coordinates, s
 
 # Model 1: Random Forrest
 # Training
-RandomForrest = RandomForestClassifier()
+RandomForrest = RandomForestClassifier(random_state=4)
 RandomForrest.fit(coord_train, step_train)
 RandomForrest_Pred = RandomForrest.predict(coord_test)
-RF_mae_train = mean_absolute_error(RandomForrest_Pred, step_test)
-print("Model 1 training MAE is: ", round(RF_mae_train,2))
+#RF_mae_train = mean_absolute_error(RandomForrest_Pred, step_test)
+#print("Model 1 training MAE is: ", round(RF_mae_train,2))
 
 
 # Cross Val,idation
-RandomForrest_cv_score = cross_val_score(RandomForrest, coord_train, step_train, cv=5, scoring='neg_mean_absolute_error')
-RF_cv_mae = -RandomForrest_cv_score.mean()
-print("Model 1 Mean Absolute Error (CV):", round(RF_cv_mae, 2))
+#RandomForrest_cv_score = cross_val_score(RandomForrest, coord_train, step_train, cv=5, scoring='neg_mean_absolute_error')
+#RF_cv_mae = -RandomForrest_cv_score.mean()
+#print("Model 1 Mean Absolute Error (CV):", round(RF_cv_mae, 2))
 
-param_grid = {
+param_grid_RF = {
      'n_estimators': [10, 30, 50],
      'max_depth': [None, 10, 20, 30],
      'min_samples_split': [2, 5, 10],
      'min_samples_leaf': [1, 2, 4],
      'max_features': ['sqrt', 'log2']
  }
-my_model3 = RandomForestClassifier(random_state=42)
-grid_search = GridSearchCV(my_model3, param_grid, cv=5, scoring='neg_mean_absolute_error', n_jobs=1)
-grid_search.fit(coord_train, step_train)
-best_params = grid_search.best_params_
-print("Best Hyperparameters:", best_params)
-best_model3 = grid_search.best_estimator_
+#my_model3 = RandomForestClassifier(random_state=42)
+grid_search_RF = GridSearchCV(RandomForrest, param_grid_RF, cv=5, scoring='f1_weighted', n_jobs=1)
+grid_search_RF.fit(coord_train, step_train)
+best_params_RF = grid_search_RF.best_params_
+print("Best Hyperparameters:", best_params_RF)
+best_model_RF = grid_search_RF.best_estimator_
+RandomForrest_Pred = best_model_RF.predict(coord_test)
 
 # Performance Anaylysis
 RF_accuracy_score = accuracy_score(step_test, RandomForrest_Pred)
@@ -93,20 +94,31 @@ RF_classification_report = classification_report(step_test, RandomForrest_Pred)
 
 print("Model 1 Performance Analysis: Random Forrest\n")
 print("Accuracy Score:", RF_accuracy_score)
-print("\nConfusion Matrix:\n", RF_confusion_matrix)
+sb.heatmap(RF_confusion_matrix)
+plt.show()
+
+#print("\nConfusion Matrix:\n", RF_confusion_matrix)
 print("\nClassification Report:\n", RF_classification_report)
 
 # Model 2: Support Vector Machine (SVM)
 SVM = SVC()
 SVM.fit(coord_train, step_train)
 SVM_pred = SVM.predict(coord_test)
-SVM_mae_train = mean_absolute_error(SVM_pred, step_test)
-print("Model 2 training MAE is: ", round(SVM_mae_train,2))
 
 # Cross Val,idation
-SVM_cv_score = cross_val_score(SVM, coord_train, step_train, cv=5, scoring='neg_mean_absolute_error')
-SVM_cv_mae = -SVM_cv_score.mean()
-print("Model 2 Mean Absolute Error (CV):", round(SVM_cv_mae, 2))
+param_grid_SVM = {
+     'C': [0.01, 0.1, 1, 10, 100],
+     'kernel': ['linear', 'poly', 'rbf'],
+     'max_iter': [100, 250, 500, 1000],
+     'class_weight': ['Balanced', None]
+ }
+
+grid_search_SVM = GridSearchCV(SVM, param_grid_SVM, cv=5, scoring='f1_weighted', n_jobs=1)
+grid_search_SVM.fit(coord_train, step_train)
+best_params_SVM = grid_search_SVM.best_params_
+print("Best Hyperparameters:", best_params_SVM)
+best_model_SVM = grid_search_SVM.best_estimator_
+SVM_Pred = best_model_SVM.predict(coord_test)
 
 # Performance Anaylysis
 SVM_accuracy_score = accuracy_score(step_test, SVM_pred)
@@ -118,26 +130,53 @@ print("Accuracy Score:", SVM_accuracy_score)
 print("\nConfusion Matrix:\n", SVM_confusion_matrix)
 print("\nClassification Report:\n", SVM_classification_report)
 
-# Model 3: Linnear Regression
-LinnearReg = LogisticRegression()
-LinnearReg.fit(coord_train, step_train)
-LinnearReg_pred = LinnearReg.predict(coord_test)
-LinnearReg_mae_train = mean_absolute_error(LinnearReg_pred, step_test)
-print("Model 3 training MAE is: ", round(LinnearReg_mae_train,2))
+# Model 3: Logistic Regression
+LogisticReg = LogisticRegression()
+LogisticReg.fit(coord_train, step_train)
+LogisticReg_pred = LogisticReg.predict(coord_test)
 
 # Cross Val,idation
-LinnearReg_cv_score = cross_val_score(LinnearReg, coord_train, step_train, cv=5, scoring='neg_mean_absolute_error')
-LinnearReg_cv_mae = -LinnearReg_cv_score.mean()
-print("Model 3 Mean Absolute Error (CV):", round(LinnearReg_cv_mae, 2))
+param_grid_LR = {
+     'C': [0.01, 0.1, 1, 10, 100],
+     'max_iter': [100, 250, 500, 1000],
+     'class_weight': ['Balanced', None]
+ }
+
+grid_search_LR = GridSearchCV(LogisticReg, param_grid_LR, cv=5, scoring='f1_weighted', n_jobs=1)
+grid_search_LR.fit(coord_train, step_train)
+best_params_LR = grid_search_LR.best_params_
+print("Best Hyperparameters:", best_params_LR)
+best_model_LR = grid_search_LR.best_estimator_
+RandomForrest_Pred = best_model_LR.predict(coord_test)
 
 # Performance Anaylysis
-LinnearReg_accuracy_score = accuracy_score(step_test, LinnearReg_pred)
-LinnearReg_confusion_matrix = confusion_matrix(step_test, LinnearReg_pred)
-LinnearReg_classification_report = classification_report(step_test, LinnearReg_pred)
+LogisticReg_accuracy_score = accuracy_score(step_test, LogisticReg_pred)
+LogisticReg_confusion_matrix = confusion_matrix(step_test, LogisticReg_pred)
+LogisticReg_classification_report = classification_report(step_test, LogisticReg_pred)
 
-print("Model 3 Performance Analysis: Linnear Regression\n")
-print("Accuracy Score:", LinnearReg_accuracy_score)
-print("\nConfusion Matrix:\n", LinnearReg_confusion_matrix)
-print("\nClassification Report:\n", LinnearReg_classification_report)
+print("Model 3 Performance Analysis: Logistic Regression\n")
+print("Accuracy Score:", LogisticReg_accuracy_score)
+print("\nConfusion Matrix:\n", LogisticReg_confusion_matrix)
+print("\nClassification Report:\n", LogisticReg_classification_report)
+
+# Model 4: RandomizedCV
+
+# Part 6: Stacked Model Performance Analysis
+    
+# Part 7: Model Evaluation
+joblib.dump(RandomForrest, 'rf_model.joblib')
+loaded_rf_model = joblib.load('rf_model.joblib')
+
+# Coordinates for prediction
+coordinates_to_predict = np.array([[9.375, 3.0625, 1.51],
+                                   [6.995, 5.125, 0.3875],
+                                   [0, 3.0625, 1.93],
+                                   [9.4, 3, 1.8],
+                                   [9.4, 3, 1.3]])
+
+# Make predictions using the loaded model for each set of coordinates
+for idx, coord_set in enumerate(coordinates_to_predict):
+    step_prediction = loaded_rf_model.predict(coord_set.reshape(1, -1))
+    print(f"Predicted Step for coordinates {idx + 1}: {step_prediction}")
 
 
